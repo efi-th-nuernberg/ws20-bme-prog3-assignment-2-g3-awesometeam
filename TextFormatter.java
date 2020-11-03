35609bb3b14d603045c66afe527f637162ceaf17
repl.it user <replituser@example.com>
2020-11-03 10:15:35 +0000

    Not Clean Yet!

--- TextFormatter.java
+++ TextFormatter.java
@@ -3,10 +3,11 @@ import java.util.StringTokenizer;
 
 class TextFormatter {
 
-	static int maxLineLength_;
-	static int letterCounter = 0;
-	static String formattedText = "";
-	static ArrayList<String> words;
+	private int maxLineLength;
+  private static int form;
+	private int letterCounter = 0;
+	private ArrayList<String> words;
+  private String right = "";
 
 	private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy "
 			+ "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et "
@@ -17,69 +18,156 @@ class TextFormatter {
 			+ "Lorem ipsum dolor sit amet.";
 
 	public static void main(String[] args) {
-		TextFormatter formatter = new TextFormatter(30);
+    if(args.length == 0){
+      form = 0;
+    }else if(args[0].toLowerCase().equals("left")){
+      form = 0;
+    }else if(args[0].toLowerCase().equals("right")){
+      form = 1;
+    }else if(args[0].toLowerCase().equals("block")){
+      form = 2;
+    }else{
+      System.out.println("Please enter one of the following commands: \"left\", \"right\" or \"block\".");
+      return;
+    }
+
+    	//0=left, 1=right, 2=block
+		TextFormatter formatter = new TextFormatter(30, form);
 		formatter.print(text);
 	}
 
 	// Konstruktor
-	public TextFormatter(int maxLineLength) {
-		maxLineLength_ = maxLineLength;
+	public TextFormatter(int maxLineLength, int form) {
+		this.form = form;
+    if(form != 3){
+      this.maxLineLength=maxLineLength;
+    }else{
+    this.maxLineLength = maxLineLength-1;
+    }
 	}
 
 	// Ausgabe
 	public void print(String text) {
 		saveWordsInList(text);
-		formatText();
-		System.out.println(formattedText);
+		System.out.println(formatText());
 	}
 
 	private void saveWordsInList(String text) {
 		words = new ArrayList<String>();
 		StringTokenizer toki = new StringTokenizer(text, " ");
-		while (toki.hasMoreTokens()) {
-			words.add(toki.nextToken());
-		}
+		while (toki.hasMoreTokens())
+			  words.add(toki.nextToken());
 	}
 
-	private void formatText() {
-		for (int i=0; i<words.size(); i++){ //iterate over all words
-			int wordLength=words.get(i).length();
-			if(wordLength>maxLineLength_){ //word is longer then the maximum linelength
-				splitWord(words.get(i));
-			}else if (letterCounter + wordLength <= maxLineLength_){ //word does fit in line 
-				formattedText += words.get(i);
-				letterCounter += wordLength;
-			} else { //word doesn´t fit in line anymore
-				letterCounter = wordLength;
-				formattedText += "\n" + words.get(i);
-			}
-			addSpaceIfNeeded();
-		}
-	}
-
-	private void splitWord(String word) {
-		formattedText += "\n";
-		letterCounter = 0;
-		for (int i = 0; i < word.length(); i++) {
-			if (letterCounter == maxLineLength_ - 1) {
-				addHyphen(i, word);
-			}
-			formattedText += word.charAt(i);
+  private String addWordWithHyphen(String word){
+		String temporary = "";
+		for (int letterPos = 0; letterPos < word.length(); letterPos++) {
+			if (letterCounter == maxLineLength){ 
+				temporary += addHyphen(letterPos, word);
+      }
+			temporary += word.charAt(letterPos);
 			letterCounter++;
-		}
-	}
+    }
+    return temporary;
+  }
 
-  private void addHyphen(int i, String word) {
-		if (word.length() != i) {
-			formattedText += "-\n";
-			letterCounter = 0;
-		}
-	}
-	
-	private void addSpaceIfNeeded(){
-	  if(letterCounter <= maxLineLength_){
-	    formattedText+= " ";
+  private String addWordWithoutHyphen(String word){
+    letterCounter+=word.length();
+    return word;
+  }
+
+  private String format(String word){
+    if(form>0){
+      return buildNewLine(word);
+    }else{
+    return word+addSpaceIfNeeded();
+    }
+  }
+
+private String buildNewLine(String word){
+  if(letterCounter+word.length() < maxLineLength){
+    letterCounter+=word.length();
+    right+=word+addSpaceIfNeeded();
+  return "";
+  }
+  letterCounter = 0;
+  String newLine = right;
+  right = word+" ";
+  letterCounter+=word.length();
+  if(form == 1){
+  return changeLetterPositions(newLine);
+  }else{
+    return changeToBlock(newLine);
+  }
+}
+
+ //Umordnen
+ private String changeLetterPositions(String newLine){
+    String temp = "";
+    for(int letterPos = -1; letterPos < (maxLineLength-newLine.length()); letterPos++){
+      temp+=" ";
+    }
+    temp+=newLine;
+    return temp + "\n";
+  }
+
+  //Block
+  private String changeToBlock(String newLine){
+    int stringLength = newLine.length();
+    int currentWord = 0;
+    while(stringLength+currentWord<maxLineLength){
+    StringTokenizer toki = new StringTokenizer(newLine.trim(), " ", true);
+    String temp="";
+    while(toki.hasMoreTokens()){
+      temp+=toki.nextToken();
+      if(stringLength+currentWord<maxLineLength && toki.hasMoreTokens()){
+        temp+=" ";
+        currentWord++;
+      }
+    }
+    newLine=temp;
+    }
+    return newLine + "\n";
+  }
+
+  private String formatText(){
+    String formattedText = "";
+    for (int wordPos=0; wordPos<words.size(); wordPos++){ //iterate over all words
+      String word = words.get(wordPos);
+      int wordLength=word.length();
+      if(form == 2 || form == 1){
+        formattedText += format(word);
+      }else if(wordLength>maxLineLength){
+        formattedText += "\n" + format(addWordWithHyphen(word));
+      }else{
+        if((wordLength+letterCounter)>maxLineLength){
+        formattedText += "\n";
+        letterCounter = 0;
+        }
+        formattedText += format(addWordWithoutHyphen(word));
+      }
+    }
+    if(form == 2){
+    formattedText+=changeToBlock(right);
+    }else if(form ==1){
+      formattedText+=changeLetterPositions(right);
+    }
+    return formattedText;
+}
+
+private String addHyphen(int letterPos, String word){
+  			letterCounter = 0;
+		if (word.charAt(0)==word.charAt(letterPos)){
+      return "\n";
+    }
+    return "-\n";
+  }
+
+  private String addSpaceIfNeeded(){
+	  if(letterCounter < maxLineLength){
       letterCounter++;
-	  }
+      return " ";
+    }
+      return "";
   }
 }
